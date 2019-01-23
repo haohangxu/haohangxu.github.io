2b5a202830f5a5aee377cabf59ea514c8f452de8
haohangxu <haohang.xu@gmail.com>
2019-01-22 21:20:42 -0500

    fix hamburger menu

--- js/main.js
+++ js/main.js
@@ -35,20 +35,23 @@ $(document).ready(function () {
               $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
           }
       })
+  }
 
 
-      if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-          $('.panel-cover').addClass('panel-cover--collapsed')
-      }
+    if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+    }
 
-      $('.btn-mobile-menu').click(function () {
-          $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-          $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-      })
+    $('.btn-mobile-menu').click(function () {
+        $('.navigation-wrapper').toggleClass('visible animated slideIn')
+        $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle fadeIn')
+    })
 
-      $('.navigation-wrapper .blog-button').click(function () {
-          $('.navigation-wrapper').toggleClass('visible')
-          $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-      })
+  for (var buttonClass in buttonActions) {
+    $('.navigation-wrapper .' + buttonClass).click(function () {
+        $('.navigation-wrapper').toggleClass('visible')
+        $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    })
+  }
 
-  }})
+})
